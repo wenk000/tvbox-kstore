e58aa5e925775e74a63bf53642c5a2c09e3253a5
haonanren118 <508965279@qq.com>
2026-05-03 16:44:47 +0800

    Implement JSON file generation with URLs
    Added functionality to generate a JSON file with URLs and metadata.

--- generate.py
+++ generate.py
@@ -1,40 +1,65 @@
+import os
+import json
+import random
+import string
+
+def random_name(n=6):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(n))
+
+content = """//阁下好手段，还请高抬贵手，放小弟一马，都是百度上谷歌来的，没啥价值，去扒别人的源吧。
+//此多仓接口列表为公众号【杰翔易达】为粉丝提供便利整理而成，如有冒犯各位接口所属大神，请联系删除。
 {
-  "spider": "",
-  "sites": [
-    {
-      "key": "libvio",
-      "name": "🔵优质线路1",
-      "api": "https://shturl.cc/G2K1aSfq360WqyMBN3dQWrIpFUyCSNxa",
-      "type": 1,
-      "searchable": 1
-    },
-    {
-      "key": "ffzy",
-      "name": "🔴优质线路2",
-      "api": "https://www.ffzy.tv/api.php/provide/vod/",
-      "type": 1,
-      "searchable": 1
-    },
-    {
-      "key": "ckzy",
-      "name": "🟠高清秒播",
-      "api": "https://www.ckzy.cc/api.php/provide/vod/",
-      "type": 1,
-      "searchable": 1
-    },
-    {
-      "key": "wuxin",
-      "name": "🟢无芯影视",
-      "api": "https://shturl.cc/o1Gt1zfqavHAATvfZgMfjysVNikaR6cGg",
-      "type": 1,
-      "searchable": 1
-    }
-  ],
-  "parses": [
-    {
-      "name": "官方解析",
-      "type": 1,
-      "url": "https://jx.jsonplayer.com/player/?url="
-    }
-  ]
-}
+    "urls": [
+         {
+            "url": "https://tv.菜妮丝.top",
+            "name": "小苹果盒子"
+        },
+        {
+            "url": "http://tvbox.王二小放牛娃.top",
+            "name": "王二小"
+        },
+        {
+            "url": "http://肥猫.com",
+            "name": "肥猫"
+        },
+        {
+            "url": "https://盒子迷.top/禁止贩卖",
+            "name": "盒子迷"
+        },
+        {
+            "url": "https://tv.菜妮丝.top",
+            "name": "菜妮丝线路"
+        },
+        {
+            "url": "https://gh-proxy.com/https://raw.githubusercontent.com/guot55/yg/main/pg/bh.json",
+            "name": "寳盒"
+        },
+        {
+            "url": "http://www.饭太硬.com/tv",
+            "name": "饭太硬1"
+        },
+        {
+            "url": "http://www.饭太硬.net/tv",
+            "name": "饭太硬2"
+        },
+        {
+            "url": "http://www.饭太硬.art/tv",
+            "name": "饭太硬3"
+        },
+        {
+            "url": "http://fty.xxooo.cf/tv",
+            "name": "饭太硬4"
+        },
+        {
+            "url": "http://fty.888484.xyz/tv",
+            "name": "饭太硬5"
+        }
+    ]
+}"""
+
+os.makedirs("download/1/tvbox", exist_ok=True)
+fname = f"download/1/tvbox/{random_name()}.json"
+with open(fname, "w", encoding="utf-8") as f:
+    f.write(content)
+
+print("✅ 生成成功:", fname)
